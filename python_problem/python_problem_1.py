b12f9a7da44a69e2713c59cc18075519f3623fd5
woony <rooneei@naver.com>
2022-07-08 15:04:27 +0900

    1-3

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,2 +1,27 @@
 num = 0
-a = int (input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :'))
+
+def StrtoInt(s):
+  try:
+    int(s)
+    return True
+  except:
+    return False
+
+
+
+while True:
+  a = input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :')
+
+  if StrtoInt(a):
+    a = int(a)
+    if a != 1 and a!= 2 and a!=3:
+      print('1,2,3 중 하나를 입력하세요')
+      continue
+    else:
+      break
+      
+  else:
+    print('정수를 입력하세요')
+    continue
+
+    
