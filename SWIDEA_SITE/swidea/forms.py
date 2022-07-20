1baa2b6c453e9b69064383ea7c635f5bc07406d8
Woony <rooneei@naver.com>
2022-07-21 05:21:00 +0900

    목록 연계

--- SWIDEA_SITE/swidea/forms.py
+++ SWIDEA_SITE/swidea/forms.py
@@ -2,9 +2,30 @@ from django import forms
 from .models import Devtool, Post
 
 class PostForm(forms.ModelForm):
-  class Meta:
-    model = Post
-    fields = ('title', 'image','content', 'interest','devtool',)
+    #tool = forms.ModelChoiceField(queryset=Tool.objects.all())
+    def __init__(self, *args, **kwargs):
+        super(PostForm, self).__init__(*args, **kwargs)
+        self.fields['title'].required = True
+        self.fields['image'].required = True
+        self.fields['content'].required = True
+        self.fields['interest'].required = True
+        self.fields['devtool'].required = True
+        
+        new_choices = []
+        tool = Devtool.objects.all()
+        for t in tool:
+            temp = []
+            temp.append(t.name)
+            temp.append(t.name)
+            temp = tuple(temp)
+            new_choices.append(temp)
+        new_choices = tuple(new_choices)
+        
+        self.fields['tool_choice'] = forms.ChoiceField(choices=new_choices, label="예상 개발 툴")
+    class Meta:
+        model = Post
+        fields = ('title', 'image','content', 'interest','devtool',)
+
 
 class PostDev(forms.ModelForm):
   class Meta:
--- SWIDEA_SITE/swidea/models.py
+++ SWIDEA_SITE/swidea/models.py
@@ -1,24 +1,28 @@
 from django.db import models
+from tabnanny import verbose
+from tkinter import CASCADE
 
 # Create your models here.
-class Post(models.Model):
-    title = models.CharField(max_length=50, verbose_name="아이디어명")
-    image = models.ImageField(blank=True, upload_to='posts/%Y%m%d', verbose_name="image")
-    content = models.TextField(verbose_name="아이디어 설명")
-    interest = models.IntegerField(verbose_name="아이디어 관심도")
-    devtool_CHOICES = (
-      ('django', 'django'),
-      ('react', 'react'),
-      ('spring', 'spring'),
-      ('node.js', 'node.js'),
-      ('java', 'java'),
-      ('C++', 'C++'),
-  )
-    devtool = models.CharField (max_length=7, choices=devtool_CHOICES, verbose_name="예상 개발툴", null = True)
-    created_at = models.DateTimeField(auto_now_add=True) 
-    updated_at = models.DateTimeField(auto_now=True)
+
 
 class Devtool(models.Model):
   name = models.CharField(max_length=50, verbose_name='이름')
   kind = models.CharField(max_length=50, verbose_name='종류')
   content = models.TextField(verbose_name="개발툴 설명")
+
+class Post(models.Model):
+    title = models.CharField(max_length=50, verbose_name="아이디어명")
+    image = models.ImageField(blank=True, upload_to='posts/%Y%m%d', verbose_name="image")
+    content = models.TextField(verbose_name="아이디어 설 명")
+    interest = models.IntegerField(verbose_name="아이디어 관심도")
+  #   devtool_CHOICES = (
+  #     ('django', 'django'),
+  #     ('react', 'react'),
+  #     ('spring', 'spring'),
+  #     ('node.js', 'node.js'),
+  #     ('java', 'java'),
+  #     ('C++', 'C++'),
+  # )
+    devtool = models.CharField(max_length=50, verbose_name="예상 개발툴", null = True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
